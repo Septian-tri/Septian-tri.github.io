aca90cf190fbf6196845a76958e8771676aa94df
Septian-tri <septiantriwidian@gmail.com>
2022-09-17 23:35:35 +0700

    init commit

---
+++ asset/js/custom.js
@@ -0,0 +1,45 @@
+$(window).on('load', function () {
+
+    var filterAktif = '.website';
+
+    $('.filters_menu li').click(function () {
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        filterAktif = data;
+
+        return iso();
+
+    });
+
+    const iso = (function isoF(){
+
+        $(".wr").isotope({
+            itemSelector: ".isoc",
+            filter: filterAktif,
+            masonry: {
+                columnWidth: ".isoc"
+            }
+        });
+
+        return isoF;
+    }());
+
+
+    $(".gambarDesainGrafis").each(function(){
+
+        $(this).on('mouseover', function(){
+
+            $(this).imageZoom({zoom : 200});
+
+        })
+
+    });
+
+});
+
+// nice select
+$(document).ready(function() {
+    $('select').niceSelect();
+  });
